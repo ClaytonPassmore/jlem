e13bf3827968d47045b6d8349865a8339529b1f6
Clayton Passmore <ctpassmore@gmail.com>
2016-10-16 22:21:11 -0400

    comma

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(name='jlem',
       version='0.1',
       description='Jupyter lab extension manager',
-      packages=['jlem']
+      packages=['jlem'],
       entry_points = {
-          'console_scripts': ['jlem=jlem:main:main']
+          'console_scripts': ['jlem=jlem.main:main']
       },
       install_requires=['jupyterlab'])
